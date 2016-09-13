cdc752627bea2b8808cef24090a18e4c421aed5a
jisudong <136225101@qq.com>
2016-09-13 23:51:26 +0800

    删除表情键盘项目

---
+++ EmoticonView/EmoticonView/EmoticonView/EmoticonPackage.swift
@@ -0,0 +1,13 @@
+//
+//  EmoticonPackage.swift
+//  EmoticonView
+//
+//  Created by 纪素东 on 16/9/12.
+//  Copyright © 2016年 纪素东. All rights reserved.
+//
+
+import UIKit
+
+class EmoticonPackage: NSObject {
+
+}
---
+++ EmoticonView/EmoticonViewTests/EmoticonViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  EmoticonViewTests.swift
+//  EmoticonViewTests
+//
+//  Created by 纪素东 on 16/9/12.
+//  Copyright © 2016年 纪素东. All rights reserved.
+//
+
+import XCTest
+@testable import EmoticonView
+
+class EmoticonViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
